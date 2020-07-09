0a9a20d26cde5cae3c88293ba67b0e9c6e249a0a
corbennett <corbennett@gmail.com>
2020-07-08 18:57:42 -0700

    save insertion coords to json

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
-conda create --name newscale python==3.7 --yes
-conda activate newscale
-pip install jupyter
-pip install pandas
-pip install matplotlib
-pip install plotly
-pip install scipy
+call conda create --name newscale python==3.7 --yes
+call conda activate newscale
+call pip install jupyter
+call pip install pandas
+call pip install matplotlib
+call pip install plotly
+call pip install scipy
---
+++ launch.bat
@@ -0,0 +1,4 @@
+call C:\mcpython3\Scripts\activate.bat C:\mcpython3\
+call conda activate newscale
+call jupyter notebook extract_newscale_coords.ipynb
+cmd /k
